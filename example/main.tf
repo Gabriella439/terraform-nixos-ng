8cd1226e7404dcff64020a8082cd30c05ce05c3a
Gabriella Gonzalez <GenuineGabriella@gmail.com>
2023-01-28 08:18:42 -0800

    Consistent indentation for example

--- example/main.tf
+++ example/main.tf
@@ -15,83 +15,83 @@ provider "aws" {
 }
 
 terraform {
-    required_version = ">= 1.3.0"
+  required_version = ">= 1.3.0"
 
-    required_providers {
-        aws = {
-           source = "hashicorp/aws"
-           version = "~> 4.16"
-        }
+  required_providers {
+    aws = {
+       source = "hashicorp/aws"
+       version = "~> 4.16"
     }
+  }
 }
 
 resource "aws_security_group" "example" {
-    # This is needed for the "nixos" module to manage the target host
-    ingress {
-        from_port = 22
+  # This is needed for the "nixos" module to manage the target host
+  ingress {
+    from_port = 22
 
-        to_port = 22
+    to_port = 22
 
-        protocol = "tcp"
+    protocol = "tcp"
 
-        cidr_blocks = [ "0.0.0.0/0" ]
-    }
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
 
-    # This is needed if you build on the target host, like this example does,
-    # so that the machine can download dependencies.  You would also need this
-    # if you were to enable the `--use-substitutes` flag for `nixos-rebuild`.
-    egress {
-        from_port = 0
+  # This is needed if you build on the target host, like this example does, so
+  # that the machine can download dependencies.  You would also need this if you
+  # were to enable the `--use-substitutes` flag for `nixos-rebuild`.
+  egress {
+    from_port = 0
 
-        to_port = 0
+    to_port = 0
 
-        protocol = "-1"
+    protocol = "-1"
 
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_key_pair" "example" {
-    public_key = file("${var.public_key_file}")
+  public_key = file("${var.public_key_file}")
 }
 
 module "ami" {
-    source = "github.com/Gabriella439/terraform-nixos-ng//ami"
+  source = "github.com/Gabriella439/terraform-nixos-ng//ami"
 
-    release = "22.11"
+  release = "22.11"
 
-    region = var.region
+  region = var.region
 }
 
 resource "aws_instance" "example" {
-    ami = module.ami.ami
+  ami = module.ami.ami
 
-    instance_type = "t3.micro"
+  instance_type = "t3.micro"
 
-    security_groups = [ aws_security_group.example.name ]
+  security_groups = [ aws_security_group.example.name ]
 
-    key_name = aws_key_pair.example.key_name
+  key_name = aws_key_pair.example.key_name
 
-    root_block_device {
-        volume_size = 7
-    }
+  root_block_device {
+    volume_size = 7
+  }
 }
 
 module "nixos" {
-    source = "github.com/Gabriella439/terraform-nixos-ng//nixos"
+  source = "github.com/Gabriella439/terraform-nixos-ng//nixos"
 
-    host = "root@${aws_instance.example.public_ip}"
+  host = "root@${aws_instance.example.public_ip}"
 
-    flake = ".#default"
+  flake = ".#default"
 
-    arguments = [
-      # You can build on another machine, including the target machine, by
-      # enabling this option, but if you build on the target machine then make
-      # sure that the firewall and security group permit outbound connections.
-      "--build-host", "root@${aws_instance.example.public_ip}",
-    ]
+  arguments = [
+    # You can build on another machine, including the target machine, by
+    # enabling this option, but if you build on the target machine then make
+    # sure that the firewall and security group permit outbound connections.
+    "--build-host", "root@${aws_instance.example.public_ip}",
+  ]
 }
 
 output "public_dns" {
-    value = aws_instance.example.public_dns
+  value = aws_instance.example.public_dns
 }
